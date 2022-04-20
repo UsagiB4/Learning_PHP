f67430f9c123e45e0ff1aff3a0708bc6e2625e0d
UsagiB4 <binaryBunny@tutanota.com>
2022-04-21 01:52:59 +0600

    Sample site

---
+++ sample.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample Site</title>
+</head>
+<body>
+    <?php 
+    include "main.html";
+    ?>
+    <div>
+        <h3>This is some content</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto, possimus iste amet aut minima fugit laborum adipisci eaque numquam assumenda. Quos et aliquid maiores voluptates dicta natus. Maxime reprehenderit fugit ea. Officiis incidunt ad voluptate dolor sequi aperiam, ullam vero recusandae at expedita aliquid consequatur laudantium tenetur. Earum, provident. Sunt consequatur voluptatibus aliquid recusandae assumenda facere rem minus veritatis a, vero odio quae, id illo doloremque porro consequuntur autem maxime natus maiores, minima at. Repellendus dolores maiores voluptatibus nam incidunt, debitis provident maxime, ipsum fuga, at inventore. Maiores repellendus cum commodi numquam provident at atque. Iusto molestiae impedit libero aliquam.</p>
+    </div>
+    <?php
+    include "footer.html";
+    ?>
+</body>
+</html>
